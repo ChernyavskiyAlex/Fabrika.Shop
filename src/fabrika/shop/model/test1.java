63a33534464f627da4790121da95dfc68e232d47
ChernyavskiyAlex <chernyavskiy.alex@gmail.com>
2014-08-01 15:22:58 +0300

    test class added

---
+++ src/fabrika/shop/model/test1.java
@@ -0,0 +1,8 @@
+package fabrika.shop.model;
+
+/**
+ * Created by alexche on 01.08.2014.
+ */
+public class test1 {
+    int test;
+}
---
+++ src/fabrika/shop/view/test2.java
@@ -0,0 +1,8 @@
+package fabrika.shop.view;
+
+/**
+ * Created by alexche on 01.08.2014.
+ */
+public class test2 {
+    int test3;
+}
